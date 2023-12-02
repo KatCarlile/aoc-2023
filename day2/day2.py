27ce0c3342196e99b8ff87fa75ccefafeb192a01
Katherine Carlile <kathrcarlile@gmail.com>
2023-12-02 10:39:31 -0500

    Day 2 part 1 and 2 initial commit

---
+++ day2/day2.py
@@ -0,0 +1,40 @@
+def read_input(filename):
+    with open(filename) as file:
+        lines = [line.rstrip() for line in file]
+        return lines
+
+def process(input, cube_cnt):
+    game_sum = 0
+    power_sum = 0
+    for game in input:
+        curr_game = int(game[game.index('e')+1 : game.index(':')])
+        max_cube_cnt = {'red':0, 'green':0, 'blue':0}
+        isValid = True
+        for rnd in game.split(';'):
+            rnd_cnt = {'red':0, 'green':0, 'blue':0}
+            for k in rnd_cnt.keys():
+                if k in rnd:
+                    rnd_cnt[k] = rnd_cnt[k] + int(rnd[rnd.index(k)-3 : rnd.index(k)-1])
+                    if rnd_cnt[k] > max_cube_cnt[k]:
+                        max_cube_cnt[k] = rnd_cnt[k]
+                    if rnd_cnt[k] > cube_cnt[k]:
+                        isValid = False
+        if isValid:
+            # print("Adding game " + str(curr_game))
+            game_sum += curr_game
+        power_sum += max_cube_cnt['red'] * max_cube_cnt['green'] * max_cube_cnt['blue']
+        # print("Game " + str(curr_game) + " played with " + str(max_cube_cnt['red'])
+        # + " red, " + str(max_cube_cnt['green']) + " green, " + str(max_cube_cnt['blue']) + " blue",
+        # "power is " + str(max_cube_cnt['red'] * max_cube_cnt['green'] * max_cube_cnt['blue']))
+    print("Power sum is " + str(power_sum))
+    print("Game sum is " + str(game_sum))
+
+def main():
+    cube_cnt = {'red':12, 'green':13, 'blue':14}
+    input_dir = r'inputs\input.txt'
+    input = read_input(input_dir)
+    process(input, cube_cnt)
+    # print(input)
+
+if __name__ == "__main__":
+    main()
